78dbdc9b8f41c3881ef7c3fe2f5a88b5cd973864
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-19 17:27:16 +0530

    Update stg_pinterest_ads__keyword_history.sql

--- models/stg_pinterest_ads__keyword_history.sql
+++ models/stg_pinterest_ads__keyword_history.sql
@@ -1,4 +1,10 @@
-{{ config(enabled=fivetran_utils.enabled_vars(['ad_reporting__pinterest_ads_enabled','pinterest__using_keywords'])) }}
+{{ config(enabled=fivetran_utils.enabled_vars(['ad_reporting__pinterest_ads_enabled','pinterest__using_keywords']),
+    partition_by={
+      "field": "_fivetran_synced", 
+      "data_type": "TIMESTAMP",
+      "granularity": "day"
+    }
+    ) }}
 
 with base as (
 
