aaf2d08de2de2ecb363d464c4a0be50419b9927e
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-19 17:26:07 +0530

    Update stg_pinterest_ads__campaign_history.sql

--- models/stg_pinterest_ads__campaign_history.sql
+++ models/stg_pinterest_ads__campaign_history.sql
@@ -1,4 +1,10 @@
-{{ config(enabled=var('ad_reporting__pinterest_ads_enabled', True)) }}
+{{ config(enabled=var('ad_reporting__pinterest_ads_enabled', True),
+    partition_by={
+      "field": "created_at", 
+      "data_type": "TIMESTAMP",
+      "granularity": "day"
+    }
+    ) }}
 
 with base as (
 
