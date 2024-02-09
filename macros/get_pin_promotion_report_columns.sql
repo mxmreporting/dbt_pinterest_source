8e0dd2c824833c096df3c2675d1bc002a8081d6a
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2024-02-09 17:32:12 +0530

    Update get_pin_promotion_report_columns.sql

--- macros/get_pin_promotion_report_columns.sql
+++ macros/get_pin_promotion_report_columns.sql
@@ -11,7 +11,10 @@
     {"name": "impression_1", "datatype": dbt.type_numeric()},
     {"name": "impression_2", "datatype": dbt.type_numeric()},
     {"name": "pin_promotion_id", "datatype": dbt.type_string()},
-    {"name": "spend_in_micro_dollar", "datatype": dbt.type_numeric()}
+    {"name": "spend_in_micro_dollar", "datatype": dbt.type_numeric()},
+    {"name": "total_lead", "datatype": dbt.type_numeric()},
+    {"name": "total_click_add_to_cart", "datatype": dbt.type_numeric()},
+    {"name": "total_checkout", "datatype": dbt.type_int()}
 ] %}
 
 {{ fivetran_utils.add_pass_through_columns(columns, var('pinterest__pin_promotion_report_passthrough_metrics')) }}
