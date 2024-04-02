c40be99c1be26388585d8577b1a6f8b9714e0c52
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2024-04-02 10:50:09 +0530

    Update stg_pinterest_ads__campaign_report.sql

--- models/stg_pinterest_ads__campaign_report.sql
+++ models/stg_pinterest_ads__campaign_report.sql
@@ -35,7 +35,8 @@ final as (
 
     select
         source_relation, 
-        CAST(FORMAT_TIMESTAMP("%F %T", {{ dbt.date_trunc('day', 'date') }}, "America/New_York") AS TIMESTAMP) as date_day,        --EST timezone conversion
+        --CAST(FORMAT_TIMESTAMP("%F %T", {{ dbt.date_trunc('day', 'date') }}, "America/New_York") AS TIMESTAMP) as date_day,        --EST timezone conversion
+        {{ dbt.date_trunc('day', 'date') }} as date_day,
         campaign_id,
         campaign_name,
         campaign_status,
