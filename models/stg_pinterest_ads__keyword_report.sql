6f22f12dbbd2fefbaf8adfe6a8da8e0ce42fbac6
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2024-04-02 11:18:30 +0530

    Update stg_pinterest_ads__keyword_report.sql

--- models/stg_pinterest_ads__keyword_report.sql
+++ models/stg_pinterest_ads__keyword_report.sql
@@ -35,7 +35,6 @@ final as (
 
     select
         source_relation,
-        --CAST(FORMAT_TIMESTAMP("%F %T", {{ dbt.date_trunc('day', 'date') }}, "America/New_York") AS TIMESTAMP) as date_day,        --EST timezone conversion
         {{ dbt.date_trunc('day', 'date') }} as date_day,
         keyword_id,
         pin_promotion_id,
